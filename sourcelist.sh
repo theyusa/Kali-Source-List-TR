1e8f6859ef8558087bcf8fb50651de42eb927322
theyusa <108272816+theyusa@users.noreply.github.com>
2024-02-24 15:50:40 +0300

    Update sourcelist.sh

--- sourcelist.sh
+++ sourcelist.sh
@@ -5,8 +5,8 @@ printf "TheYusa\n"
 printf "Adding sourceslist Turkey...\n"
 
 
-echo "deb http://kali.download/kali kali-last-snapshot main contrib non-free" | sudo  tee /etc/apt/sources.list
-echo "deb http://kali.download/kali kali-rolling main contrib non-free" | sudo  tee  -a /etc/apt/sources.list
+echo "deb http://kali.download/kali kali-last-snapshot main contrib non-free non-free-firmware" | sudo  tee /etc/apt/sources.list
+echo "deb http://kali.download/kali kali-rolling main contrib non-free non-free-firmware" | sudo  tee  -a /etc/apt/sources.list
 printf "Adding successful\n"
 printf "Updating and upgrading...\n"
 
